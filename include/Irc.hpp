078aa4bcac17bc582a735ee858589562c4e33a84
urabexon <hiroki708.a48uk@gmail.com>
2025-05-19 21:41:48 +0900

    Inc.hpp indent fix

--- include/Irc.hpp
+++ include/Irc.hpp
@@ -6,7 +6,7 @@
 /*   By: urabex <urabex@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 01:17:58 by urabex            #+#    #+#             */
-/*   Updated: 2025/05/16 01:40:41 by urabex           ###   ########.fr       */
+/*   Updated: 2025/05/19 21:40:50 by urabex           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,22 +32,22 @@
 
 // ----- Error masege -----
 // fatal errors
-#define ERROR_OPEN_FILE			"Error: could not open file"
+#define ERROR_OPEN_FILE			    "Error: could not open file"
 #define ERROR_FILE_EMPTY			"Error: file is empty"
-#define ERROR_FILE_CONTENTS		"Error: file is wrong"
-#define ERROR_SERVER_SOCKET		"Error: socket creation failed"
+#define ERROR_FILE_CONTENTS		    "Error: file is wrong"
+#define ERROR_SERVER_SOCKET		    "Error: socket creation failed"
 #define ERROR_SERVER_SETSOCKETOPT	"Error: setsockopt failed"
 #define ERROR_SERVER_BIND			"Error: bind failed on port "
-#define ERROR_SERVER_LISTEN		"Error: listen failed"
+#define ERROR_SERVER_LISTEN		    "Error: listen failed"
 // normal errors
-#define ERROR_SERVER_ACCEPT		"Error: accept failed"
+#define ERROR_SERVER_ACCEPT		    "Error: accept failed"
 #define ERROR_SERVER_FULL			"Error: server is already full"
 #define ERROR_CLIENT_NOT_FOUND		"Error: client not found #"
 #define ERROR_SERVER_RECV			"Error: recv failed"
 #define ERROR_CLIENT_INVALID_INFO	"Error: invalid client information"
 #define ERROR_SERVER_POLL			"Error: poll failed on fd: "
-#define ERROR_CHANNEL_EXIST		"Error: Channel already exists #"
-#define ERROR_CHANNEL_NOT_FOUND	"Error: Channel not found #"
+#define ERROR_CHANNEL_EXIST		    "Error: Channel already exists #"
+#define ERROR_CHANNEL_NOT_FOUND	    "Error: Channel not found #"
 #define ERROR_SERVER_EMPTY_CMD		"Error: empty command"
 
 // Server message
