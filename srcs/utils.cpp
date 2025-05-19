10cefddf94cd60f7bf6dfd29533adaccf4b836bc
urabexon <hiroki708.a48uk@gmail.com>
2025-05-19 22:30:45 +0900

    add comment

--- srcs/utils.cpp
+++ srcs/utils.cpp
@@ -6,21 +6,24 @@
 /*   By: urabex <urabex@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 22:22:13 by urabex            #+#    #+#             */
-/*   Updated: 2025/05/19 22:26:09 by urabex           ###   ########.fr       */
+/*   Updated: 2025/05/19 22:30:26 by urabex           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Irc.hpp"
 #include "Server.hpp"
 
+// クライアントの送信バッファにメッセージを追加する関数
 void addToClientSendBuf(Server *server, int clientFd, std::string message) {
     
 }
 
+// 指定されたFDに対応するクライアントを取得する関数
 Client &retrieveClient(Server *server, int clientFd) {
     
 }
 
+// メッセージ文字列からチャンネル名を抽出する関数
 std::string getChannelName(std::string &msgToParse) {
 
 }
@@ -30,6 +33,7 @@ std::string trim(const std::string &s) {
     
 }
 
+// どの種類のIRCコマンドか判定、コマンドタイプの番号を返す
 int getCommandType(std::string &cmd) {
     int	type = 0;
 
