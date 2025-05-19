bec5c59212beff062affadba5e96230a78e3b691
urabexon <hiroki708.a48uk@gmail.com>
2025-05-19 23:19:57 +0900

    trim トリム追加

--- srcs/utils.cpp
+++ srcs/utils.cpp
@@ -6,7 +6,7 @@
 /*   By: urabex <urabex@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 22:22:13 by urabex            #+#    #+#             */
-/*   Updated: 2025/05/19 23:17:59 by urabex           ###   ########.fr       */
+/*   Updated: 2025/05/19 23:19:34 by urabex           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,6 @@ void addToClientSendBuf(Server *server, int clientFd, std::string message) {
 Client &retrieveClient(Server *server, int clientFd) {
 	std::map<const int, Client>& clientList = server->getClientList();
 	std::map<const int, Client>::iterator it = clientList.find(clientFd);
-
 	Client &client = it->second;
 	return (client);
 }
@@ -36,7 +35,11 @@ std::string getChannelName(std::string &msgToParse) {
 
 // 文字列の先頭と末尾の空白（スペース、タブ）を除去する
 std::string trim(const std::string &s) {
-    
+    std::string::size_type start = s.find_first_not_of(" \t\r\n");
+	if (start == std::string::npos)
+		return "";
+	std::string::size_type end = s.find_last_not_of(" \t\r\n");
+	return s.substr(start, end - start + 1);
 }
 
 // どの種類のIRCコマンドか判定、コマンドタイプの番号を返す
