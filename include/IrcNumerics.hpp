b871d580f704148d37d206c63225c05ca5086c02
urabexon <hiroki708.a48uk@gmail.com>
2025-05-19 21:27:57 +0900

    IncNumerics.hpp add success message

--- include/IrcNumerics.hpp
+++ include/IrcNumerics.hpp
@@ -6,7 +6,7 @@
 /*   By: urabex <urabex@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 01:18:00 by urabex            #+#    #+#             */
-/*   Updated: 2025/05/19 21:25:07 by urabex           ###   ########.fr       */
+/*   Updated: 2025/05/19 21:27:29 by urabex           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,6 +51,26 @@
 #define PRIVMSG_REQUIREMENTS    "[!] message requirements: len(400)\r\n"
 #define PART_USAGE			    "Usage: PART <channel> [<reason>]\r\n"
 #define PART_REQUIREMENTS	    "[!] reason requirements: len(400)\r\n"
+
+// Success message
+#define IRC_PREFIX(nickname, username) (":" + nickname + "!" + username + "@localhost")
+#define RPL_PASS                "Password accepted\r\n"
+#define RPL_USER(IRC_PREFIX, user, real) (IRC_PREFIX + " :Your username is " + user + ", realname is " + real + "\r\n")
+#define RPL_NICK(IRC_PREFIX, newNick) (IRC_PREFIX + " NICK " + newNick + "\r\n")
+#define RPL_JOIN(IRC_PREFIX, channel) (IRC_PREFIX + " JOIN :#" + channel + "\r\n")
+#define RPL_TOPICSET(IRC_PREFIX, channel, topic) (IRC_PREFIX + " TOPIC #" + channel + " " + topic + "\r\n")
+#define RPL_INVITE(IRC_PREFIX, target, channelName) (IRC_PREFIX + " INVITE " + target + " #" + channelName + "\r\n")
+#define RPL_KICK(IRC_PREFIX, channel, target, comment) (IRC_PREFIX + " KICK #" + channel + " " + target + " " + comment + "\r\n")
+#define RPL_MODE(IRC_PREFIX, channel, modestring) (IRC_PREFIX + " MODE #" + channel + " " + modestring + "\r\n")
+#define RPL_PRIVMSG(IRC_PREFIX, target, message) (IRC_PREFIX + " PRIVMSG " + target + " " + message + "\r\n")
+#define RPL_PART(IRC_PREFIX, channel, reason) (IRC_PREFIX + " PART #" + channel + " " + reason + "\r\n")
+#define RPL_QUIT(IRC_PREFIX, reason) (IRC_PREFIX + " :QUIT:" + reason + "\r\n")
+
+#define DEFAULT_KICK_COMMENT	":Kicked by channel operator"
+#define DELIMITER_LINE          "-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-\r\n"
+
+
+
 
 
 #endif
