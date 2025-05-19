9638453f77b153b47a725843228e4e62d2b3695c
urabexon <hiroki708.a48uk@gmail.com>
2025-05-19 21:49:43 +0900

    Server.cpp add Constructor Destructor

--- srcs/Server.cpp
+++ srcs/Server.cpp
@@ -6,7 +6,26 @@
 /*   By: urabex <urabex@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 01:19:36 by urabex            #+#    #+#             */
-/*   Updated: 2025/05/16 01:19:37 by urabex           ###   ########.fr       */
+/*   Updated: 2025/05/19 21:48:59 by urabex           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "Server.hpp"
+
+Server::Server(std::string port, std::string password, struct tm *timeinfo) : _serverSockFd(-1), _port(port), _password(password), _clientList(), _channelList(), _serverInfo(NULL) {
+    std::cout << "[Server] Server running..." << std::endl;
+    
+    // 時間を文字列として取得
+    char buf[80];
+    strftime(buf, 80, "%Y-%m-%d %H:%M:%S", timeinfo);
+    _dateTime = std::string(buf);
+
+    // サーバー初期設定
+    memset(&_addrInfo, 0, sizeof(_addrInfo));
+    _addrInfo.ai_family = AF_INET;       // IPv4
+    _addrInfo.ai_socktype = SOCK_STREAM; // TCP
+    _addrInfo.ai_flags = AI_PASSIVE;     // ローカルホストで動作
+}
+
+Server::~Server() {}
 
