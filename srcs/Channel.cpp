cdc5832db34d799b871fe00e1a1f2a37276663a5
urabexon <hiroki708.a48uk@gmail.com>
2025-05-20 16:11:27 +0900

    Channel.cpp add setMode

--- srcs/Channel.cpp
+++ srcs/Channel.cpp
@@ -6,7 +6,7 @@
 /*   By: urabex <urabex@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 12:50:53 by urabex            #+#    #+#             */
-/*   Updated: 2025/05/20 12:51:42 by urabex           ###   ########.fr       */
+/*   Updated: 2025/05/20 16:10:56 by urabex           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,3 +20,35 @@ Channel::Channel(const std::string &name): _name(name), _topic(""), _password(""
 }
 
 Channel::~Channel() {}
+
+void Channel::setTopic(const std::string &topic) { 
+    _topic = topic;
+}
+
+void Channel::setPassword(const std::string &password) {
+    _password = password;
+}
+
+void Channel::setCapacity(int capacity) {
+    _capacity = capacity;
+}
+
+void Channel::setMode(bool sign, const char mode) {
+    switch (mode) {
+        case 'i':
+			_mode.invite = sign;
+			break;
+		case 't':
+			_mode.topic = sign;
+			break;
+		case 'k':
+			_mode.key = sign;
+			break;
+		case 'l':
+			_mode.limit = sign;
+			break;
+		default:
+			std::cerr << "invalid option" << std::endl;
+			return ;
+    }
+}
