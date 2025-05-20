80d98f3c9a97bb65cdc761b4c05938c64bf86f69
urabexon <hiroki708.a48uk@gmail.com>
2025-05-20 18:01:43 +0900

    Channel.cpp add client func

--- srcs/Channel.cpp
+++ srcs/Channel.cpp
@@ -6,7 +6,7 @@
 /*   By: urabex <urabex@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 12:50:53 by urabex            #+#    #+#             */
-/*   Updated: 2025/05/20 16:13:28 by urabex           ###   ########.fr       */
+/*   Updated: 2025/05/20 18:01:00 by urabex           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,5 +71,53 @@ size_t Channel::getCapacity() const {
 
 std::map<const int, Client> &Channel::getClientList() {
     return (_clientList);
+}
+
+bool Channel::getMode(const std::string mode) const {
+    if (mode == "i" || mode == "invite")
+		return (_mode.invite);
+	else if (mode == "t" || mode == "topic")
+		return (_mode.topic);
+	else if (mode == "k" || mode == "key")
+		return (_mode.key);
+	else if (mode == "l" || mode == "limit")
+		return (_mode.limit);
+	else
+		return (false);
+}
+
+bool Channel::isClientInChannel(const int clientFd) {
+    std::map<const int, Client>::iterator it = _clientList.find(clientFd);
+	if (it == _clientList.end())
+		return (false);
+	return (true);
+}
+
+bool Channel::isInvited(const int clientFd) {
+	for (std::vector<int>::iterator it = _invitedList.begin(); it != _invitedList.end(); ++it) {
+		if (*it == clientFd)
+			return (true);
+	}
+	return (false);
+}
+
+bool Channel::isOperator(const int clientFd) {
+	for (std::vector<int>::iterator it = _operatorList.begin(); it != _operatorList.end(); ++it) {
+		if (*it == clientFd)
+			return (true);
+	}
+	return (false);
+}
+
+void Channel::addClientToChannel(Client &client) {
+    _clientList.insert(std::make_pair(client.getClientFd(), client));
+}
+
+void Channel::addOperatorList(int clientFd) {
+    _operatorList.push_back(clientFd);
+}
+
+void Channel::addInvitedList(int clientFd) {
+    _invitedList.push_back(clientFd);
 }
 
